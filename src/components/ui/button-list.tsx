0805261d585dfd5f98844bdf3e70f7c464c6c01a
Jae-seok Jo (조재석) <whwotjr98@naver.com>
2025-09-23 21:45:07 +0900

    feat: ButtonList 컴포넌트 제작 (#13)
    * feat: tailwind 사용을 위한 유틸함수 추가
    
    * feat: Tailwind 중복 스타일링 방지를 위한 유틸함수 추가
    
    * className 타입 추가
    
    * feat: extendTailwindMerge에 font-size 그룹 추가
    
    * feat: BottomCta 컴포넌트 제작
    
    * feat: add check icon SVG
    
    * feat: ButtonList 컴포넌트와 Item 컴포넌트 추가
    
    * chore: pointer 스타일 추가
    
    * feat: 트랜지션 이벤트 추가
    
    * fix: title text 색상 수정
    
    * a11y: button 내 올바르지 않은 태그 수정

---
+++ src/components/ui/button-list.tsx
@@ -0,0 +1,66 @@
+import Image from 'next/image';
+import { ButtonHTMLAttributes, PropsWithChildren } from 'react';
+
+import { cn } from '@/lib/utils';
+import { ClassName } from '@/types/common';
+
+interface ButtonListProps extends ClassName, PropsWithChildren {}
+interface ButtonListItemProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  title: string;
+  subTitle?: string;
+  selected?: boolean;
+}
+
+const ButtonList = ({ children, className }: ButtonListProps) => {
+  return <ul className={cn('flex flex-col gap-3', className)}>{children}</ul>;
+};
+
+const Item = ({
+  title,
+  subTitle,
+  selected = false,
+  className,
+  ...props
+}: ButtonListItemProps) => {
+  return (
+    <li>
+      <button
+        {...props}
+        role='option'
+        aria-selected={selected}
+        className={cn(
+          'flex w-full cursor-pointer items-center justify-between rounded-xl border p-2.5 pr-5 transition-all duration-75 ease-out active:scale-[0.98]',
+          selected ? 'border-[#3491FF]' : 'border-[#00000010]',
+          className
+        )}
+      >
+        <span className='flex items-center gap-2.5 p-2.5'>
+          <span
+            className={cn(
+              'text-[#292929]',
+              selected ? 'font-bold' : 'font-medium'
+            )}
+          >
+            {title}
+          </span>
+          {subTitle && (
+            <span className='text-sm font-normal text-[#555]'>{subTitle}</span>
+          )}
+        </span>
+        {/* TODO: 나타날 때, 인터렉션이 있으면 좋을 것 같은데 고려 필요 */}
+        {selected && (
+          <Image
+            src='/icons/check.svg'
+            width={24}
+            height={24}
+            alt=''
+          />
+        )}
+      </button>
+    </li>
+  );
+};
+
+ButtonList.Item = Item;
+
+export default ButtonList;
