5b773f3d73218251f56ea562fbbab34f8505289e
Pridesd <whwotjr98@naver.com>
2025-09-23 21:28:32 +0900

    feat: BottonCTA 리팩토링, Button을 자유롭게 나열할 수 있게 함

--- src/components/ui/bottom-cta.tsx
+++ src/components/ui/bottom-cta.tsx
@@ -1,16 +1,17 @@
-import { ButtonHTMLAttributes, PropsWithChildren } from 'react';
+import {
+  ButtonHTMLAttributes,
+  Children,
+  cloneElement,
+  isValidElement,
+  PropsWithChildren,
+  ReactElement,
+} from 'react';
 
 import { cn } from '@/lib/utils';
 import { ClassName } from '@/types/common';
 
-interface BottomCTAProps extends ClassName, PropsWithChildren {}
-
-interface BottomCTASingleProps extends ClassName, BottomCTAProps {}
-
-interface BottomCTADoubleProps extends ClassName {
-  ratio: '1:1' | '1:2';
-  leftButtonProps: BottomCTAButtonProps;
-  rightButtonProps: BottomCTAButtonProps;
+interface BottomCTAProps extends ClassName, PropsWithChildren {
+  ratio?: '1:1' | '1:2';
 }
 
 interface BottomCTAButtonProps extends ClassName, PropsWithChildren {
@@ -22,52 +23,32 @@ interface BottomCTAButtonProps extends ClassName, PropsWithChildren {
 }
 
 const BOTTOM_CTA_CLASSNAME = 'fixed right-0 bottom-0 left-0';
-// TODO: 너비 확정 시 적용 필요
 const BOTTOM_CTA_INNER_CLASSNAME = 'mx-auto h-14 w-full';
 
-const BottomCTA = ({ children, className }: BottomCTAProps) => {
-  return (
-    <footer className={BOTTOM_CTA_CLASSNAME}>
-      <div className={cn(BOTTOM_CTA_INNER_CLASSNAME, className)}>
-        {children}
-      </div>
-    </footer>
-  );
-};
+const BottomCTA = ({ ratio = '1:1', children, className }: BottomCTAProps) => {
+  const validButtons = Children.toArray(children).filter(child =>
+    isValidElement(child)
+  ) as ReactElement<BottomCTAButtonProps>[];
 
-const Single = ({ children, className }: BottomCTASingleProps) => {
+  const buttonCount = validButtons.length;
   return (
     <footer className={BOTTOM_CTA_CLASSNAME}>
       <div className={cn(BOTTOM_CTA_INNER_CLASSNAME, className)}>
-        {children}
-      </div>
-    </footer>
-  );
-};
+        {validButtons.map((button, index) => {
+          let widthClassName = 'w-full';
 
-const Double = ({
-  ratio,
-  className,
-  leftButtonProps,
-  rightButtonProps,
-}: BottomCTADoubleProps) => {
-  return (
-    <footer className={BOTTOM_CTA_CLASSNAME}>
-      <div className={cn(BOTTOM_CTA_INNER_CLASSNAME, className)}>
-        <Button
-          {...leftButtonProps}
-          className={cn(
-            ratio === '1:1' ? 'w-1/2' : 'w-1/3',
-            leftButtonProps.className
-          )}
-        />
-        <Button
-          {...rightButtonProps}
-          className={cn(
-            ratio === '1:1' ? 'w-1/2' : 'w-2/3',
-            rightButtonProps.className
-          )}
-        />
+          if (buttonCount === 2) {
+            if (index === 0) {
+              widthClassName = ratio === '1:1' ? 'w-1/2' : 'w-1/3';
+            } else {
+              widthClassName = ratio === '1:1' ? 'w-1/2' : 'w-2/3';
+            }
+          }
+
+          return cloneElement(button, {
+            className: cn(widthClassName, button.props.className),
+          });
+        })}
       </div>
     </footer>
   );
@@ -82,7 +63,6 @@ const BUTTON_VARIANT_CLASSNAME = {
 
 const Button = ({
   children,
-  label,
   variant,
   className,
   disabled,
@@ -95,18 +75,16 @@ const Button = ({
       disabled={disabled}
       className={cn(
         BUTTON_VARIANT_CLASSNAME[variant],
-        'text-18-semibold h-14 w-full rounded-none transition-all duration-100 ease-out',
+        'text-18-semibold h-14 rounded-none transition-all duration-100 ease-out',
         className
       )}
       onClick={onClick}
     >
-      {label ? label : children}
+      {children}
     </button>
   );
 };
 
-BottomCTA.Single = Single;
 BottomCTA.Button = Button;
-BottomCTA.Double = Double;
 
 export default BottomCTA;
