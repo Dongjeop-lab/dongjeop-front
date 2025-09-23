3eca99741700489a0b8ad0f98c68c09ae8f4f64a
Pridesd <whwotjr98@naver.com>
2025-09-23 19:55:31 +0900

    a11y: button 내 올바르지 않은 태그 수정

--- src/components/ui/button-list.tsx
+++ src/components/ui/button-list.tsx
@@ -34,19 +34,19 @@ const Item = ({
           className
         )}
       >
-        <div className='flex items-center gap-2.5 p-2.5'>
-          <h1
+        <span className='flex items-center gap-2.5 p-2.5'>
+          <span
             className={cn(
               'text-[#292929]',
               selected ? 'font-bold' : 'font-medium'
             )}
           >
             {title}
-          </h1>
+          </span>
           {subTitle && (
-            <p className='text-sm font-normal text-[#555]'>{subTitle}</p>
+            <span className='text-sm font-normal text-[#555]'>{subTitle}</span>
           )}
-        </div>
+        </span>
         {/* TODO: 나타날 때, 인터렉션이 있으면 좋을 것 같은데 고려 필요 */}
         {selected && (
           <Image
