d79c7ad7a0875536d324af1ef053a6bd18eec245
Jae-seok Jo (조재석) <whwotjr98@naver.com>
2025-09-23 21:43:16 +0900

    feat: BottomCTA 컴포넌트 제작 (#12)
    * feat: tailwind 사용을 위한 유틸함수 추가
    
    * feat: Tailwind 중복 스타일링 방지를 위한 유틸함수 추가
    
    * className 타입 추가
    
    * feat: extendTailwindMerge에 font-size 그룹 추가
    
    * feat: BottomCta 컴포넌트 제작
    
    * feat: BottonCTA 리팩토링, Button을 자유롭게 나열할 수 있게 함
    
    * refactor: 버튼 width 상수화
    
    * chore: 불필요한 주석 제거
    
    * feat: 버튼이 3개 이상인 경우에도 대응되도록 함

---
+++ src/components/ui/bottom-cta.tsx
@@ -0,0 +1,94 @@
+import {
+  ButtonHTMLAttributes,
+  Children,
+  cloneElement,
+  isValidElement,
+  PropsWithChildren,
+  ReactElement,
+} from 'react';
+
+import { cn } from '@/lib/utils';
+import { ClassName } from '@/types/common';
+
+interface BottomCTAProps extends ClassName, PropsWithChildren {
+  ratio?: '1:1' | '1:2';
+}
+
+interface BottomCTAButtonProps extends ClassName, PropsWithChildren {
+  type?: ButtonHTMLAttributes<HTMLButtonElement>['type'];
+  variant: 'primary' | 'secondary';
+  disabled?: boolean;
+  label?: string;
+  onClick?: VoidFunction;
+}
+
+const BOTTOM_CTA_CLASSNAME = 'fixed right-0 bottom-0 left-0';
+const BOTTOM_CTA_INNER_CLASSNAME = 'flex mx-auto h-14 w-full';
+const BUTTON_WIDTHS = {
+  '1:1': ['w-1/2', 'w-1/2'],
+  '1:2': ['w-1/3', 'w-2/3'],
+} as const;
+
+const BottomCTA = ({ ratio = '1:1', children, className }: BottomCTAProps) => {
+  const validButtons = Children.toArray(children).filter(child =>
+    isValidElement(child)
+  ) as ReactElement<BottomCTAButtonProps>[];
+
+  const buttonCount = validButtons.length;
+  return (
+    <footer className={BOTTOM_CTA_CLASSNAME}>
+      <div className={cn(BOTTOM_CTA_INNER_CLASSNAME, className)}>
+        {validButtons.map((button, index) => {
+          let widthClassName: string;
+
+          if (buttonCount === 1) {
+            widthClassName = 'w-full';
+          } else if (buttonCount === 2) {
+            widthClassName = BUTTON_WIDTHS[ratio][index];
+          } else {
+            widthClassName = 'flex-1';
+          }
+
+          return cloneElement(button, {
+            className: cn(widthClassName, button.props.className),
+          });
+        })}
+      </div>
+    </footer>
+  );
+};
+
+const BUTTON_VARIANT_CLASSNAME = {
+  primary:
+    'bg-primary text-primary-foreground active:bg-primary-pressed active:text-primary-pressed-foreground disabled:bg-primary-disabled disabled:text-primary-disabled-foreground',
+  secondary:
+    'bg-secondary text-secondary-foreground active:bg-secondary-pressed active:text-secondary-pressed-foreground disabled:bg-secondary-disabled disabled:text-secondary-disabled-foreground',
+} as const;
+
+const Button = ({
+  children,
+  variant,
+  className,
+  disabled,
+  type = 'button',
+  onClick,
+}: BottomCTAButtonProps) => {
+  return (
+    <button
+      type={type}
+      disabled={disabled}
+      className={cn(
+        BUTTON_VARIANT_CLASSNAME[variant],
+        'text-18-semibold h-14 rounded-none transition-all duration-100 ease-out',
+        className
+      )}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+BottomCTA.Button = Button;
+
+export default BottomCTA;
---
+++ src/lib/utils.ts
@@ -0,0 +1,42 @@
+import clsx from 'clsx';
+import { ClassValue } from 'clsx';
+import { extendTailwindMerge } from 'tailwind-merge';
+
+export const extendTwMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      'font-size': [
+        'text-26-bold',
+        'text-18-semibold',
+        'text-18-medium',
+        'text-16-regular',
+      ],
+      'text-color': [
+        'text-primary',
+        'text-primary-foreground',
+        'text-primary-pressed',
+        'text-primary-pressed-foreground',
+        'text-primary-disabled',
+        'text-primary-disabled-foreground',
+        'text-secondary',
+        'text-secondary-foreground',
+        'text-secondary-pressed',
+        'text-secondary-pressed-foreground',
+        'text-secondary-disabled',
+        'text-secondary-disabled-foreground',
+      ],
+      'bg-color': [
+        'bg-primary',
+        'bg-primary-pressed',
+        'bg-primary-disabled',
+        'bg-secondary',
+        'bg-secondary-pressed',
+        'bg-secondary-disabled',
+      ],
+    },
+  },
+});
+
+export const cn = (...inputs: ClassValue[]) => {
+  return extendTwMerge(clsx(inputs));
+};
