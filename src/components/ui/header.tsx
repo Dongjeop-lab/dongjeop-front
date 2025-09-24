41ef10a7aa32e9e5ed267474710eff27014563a6
Pridesd <whwotjr98@naver.com>
2025-09-24 23:28:03 +0900

    feat: header 컴포넌트 제작

---
+++ src/components/ui/header.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+
+interface HeaderProps {
+  onBack: VoidFunction;
+  onClose?: VoidFunction;
+}
+
+const Header = ({ onBack, onClose }: HeaderProps) => {
+  return (
+    <>
+      <header className='absolute top-0 left-0 flex h-11 w-full items-center justify-between px-4'>
+        <button onClick={onBack}>
+          <Image
+            src='/icons/navigation-back.svg'
+            alt='뒤로 가기'
+            width={24}
+            height={24}
+          />
+        </button>
+        <button onClick={onClose}>
+          <Image
+            src='/icons/navigation-close.svg'
+            alt='닫기'
+            width={24}
+            height={24}
+          />
+        </button>
+      </header>
+      <div className='h-11' />
+    </>
+  );
+};
+
+export default Header;
