a54b88e7612b17f07438fa8072158f129e70b6e1
Pridesd <whwotjr98@naver.com>
2025-09-25 20:41:44 +0900

    feat: step3 컴포넌트 제작

---
+++ src/features/label/components/label-step/label-step3.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import { useState } from 'react';
+
+import ButtonList from '@/components/ui/button-list';
+import Header from '@/components/ui/header';
+import { TOTAL_LABELING_STEPS } from '@/lib/constants';
+
+import { labelOption } from '../../types/label-option';
+import LabelStepLayout from '../label-step-layout';
+
+const LABEL_STEP_3_OPTIONS: labelOption[] = [
+  {
+    title: '좁아요',
+    subtitle: '2명이 동시에 지나갈 수 있어요',
+    value: 'narrow',
+  },
+  {
+    title: '보통이에요',
+    subtitle: '3명이 동시에 지나갈 수 있어요',
+    value: 'normal',
+  },
+  {
+    title: '넓어요',
+    subtitle: '4명이 동시에 지나갈 수 있어요',
+    value: 'wide',
+  },
+  {
+    title: '휠체어 진입 불가',
+    value: 'impossible',
+  },
+  {
+    title: '잘 모르겠어요',
+    value: 'not_sure',
+  },
+];
+
+export const LabelStep3 = () => {
+  const [selectedValue, setSelectedValue] = useState<string | null>(null);
+  return (
+    <>
+      <Header onBack={() => null} />
+      <LabelStepLayout
+        title='통로 폭을 알려주세요'
+        description={`통로가 많다면 가장 많이 보이는\n폭을 선택해주세요`}
+        currentStep={3}
+        totalSteps={TOTAL_LABELING_STEPS}
+        imageKey='/images/dummy-label-image.png'
+        labelingOptions={
+          <ButtonList className='w-full'>
+            {LABEL_STEP_3_OPTIONS.map(({ title, subtitle, value }) => (
+              <ButtonList.Item
+                key={title}
+                title={title}
+                subTitle={subtitle}
+                selected={selectedValue === value}
+                onClick={() => setSelectedValue(value)}
+              />
+            ))}
+          </ButtonList>
+        }
+      />
+    </>
+  );
+};
