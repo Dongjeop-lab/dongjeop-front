bc95057c0e9d5ea6f39d2d3ad9777f1d69cb9820
Hyerim Yang <lovelimi1113@gmail.com>
2025-09-28 00:16:11 +0900

    feat: 루트 경로에서 '/label/upload'로 리다이렉트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,5 @@
+import { redirect } from 'next/navigation';
+
 export default function Home() {
-  return (
-    <>
-      <p className='text-26-bold'>Pretendard 26px Bold Letter spacing -2%</p>
-      <p className='text-18-semibold'>
-        Pretendard 18px Medium Letter spacing -1.5%
-      </p>
-      <p className='text-18-medium'>
-        Pretendard 18px Medium Letter spacing -1.5%
-      </p>
-      <p className='text-16-regular'>
-        Pretendard 16px Regular Letter spacing -2%
-      </p>
-      <p>default text style</p>
-    </>
-  );
+  return redirect('/label/upload');
 }
