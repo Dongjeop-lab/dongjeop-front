0aaf19870d06caab16f02e03eb71b17e7dceb81d
Hyerim Yang <lovelimi1113@gmail.com>
2025-10-01 02:24:52 +0900

    feat: 등록완료화면-1 UI 구현

--- src/app/label/[imageKey]/finish/_components/finish-step-1.tsx
+++ src/app/label/[imageKey]/finish/_components/finish-step-1.tsx
@@ -1,9 +1,45 @@
+import Image from 'next/image';
+
 interface FinishStep1Props {
   seqNo: number;
 }
 
 const FinishStep1 = ({ seqNo }: FinishStep1Props) => {
-  return <div>FinishStep1</div>;
+  return (
+    <>
+      {/* XXX: Status bar 높이까지 section의 padding-top에 포함한 상태 */}
+      <section className='flex w-[360px] flex-1 flex-col items-center justify-center px-12.5 pt-24.5'>
+        <div className='flex flex-col items-center gap-y-2.5 pt-7.5 pb-5'>
+          <p className='text-18-semibold text-primary'>사진 등록 완료!</p>
+          <h2 className='text-26-bold text-center'>
+            {seqNo}번째 기여자세요!
+            <br />
+            함께해 주셔서 감사해요.
+          </h2>
+        </div>
+
+        <Image
+          src='/images/finish/stairs-finish-2.svg'
+          className='relative my-7.5 overflow-hidden rounded-2xl'
+          alt=''
+          aria-hidden='true'
+          width={280}
+          height={260}
+        />
+      </section>
+
+      {/* 반원 그라데이션 */}
+      <div className='relative w-full flex-1'>
+        <div
+          className='absolute -bottom-18 left-1/2 h-36 w-100 -translate-x-1/2 bg-[#ff8a00]'
+          style={{
+            borderRadius: 'borderRadius: "50% / 100% 100% 0 0"',
+            filter: 'blur(180px)',
+          }}
+        />
+      </div>
+    </>
+  );
 };
 
 export default FinishStep1;
