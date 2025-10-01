e2ef90c6238fa6972a13435de3d38646b164bd99
Hyerim Yang <lovelimi1113@gmail.com>
2025-10-01 21:26:30 +0900

    feat: 라벨링 및 등록완료 경로 추가

--- src/lib/path.ts
+++ src/lib/path.ts
@@ -1,6 +1,8 @@
 export const BROWSER_PATH = {
   LABEL: {
     UPLOAD: '/label/upload',
+    LABELING: (imageKey: string) => `/label/${imageKey}`,
+    FINISH: (imageKey: string) => `/label/${imageKey}/finish`,
   },
 } as const;
 
@@ -9,4 +11,5 @@ const DEFAULT_API_ENDPOINT = '/api/v1';
 export const API_PATH = {
   LABEL: `${DEFAULT_API_ENDPOINT}/label`,
   UPLOAD: `${DEFAULT_API_ENDPOINT}/image`,
+  FINISH: `${DEFAULT_API_ENDPOINT}/submissions/result`,
 } as const;
