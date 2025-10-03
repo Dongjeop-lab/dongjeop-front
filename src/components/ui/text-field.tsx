8c7e9f17c876a3c863163ac9db94e0f6a562fc00
Jae-seok Jo (조재석) <whwotjr98@naver.com>
2025-10-03 14:45:47 +0900

    feat: textfield 컴포넌트 제작 (#35)
    * feat: textfield 컴포넌트 제작
    
    * feat: 색상 클래스화

---
+++ src/components/ui/text-field.tsx
@@ -0,0 +1,25 @@
+import { InputHTMLAttributes } from 'react';
+
+import { cn } from '@/lib/utils';
+
+interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
+  type?: 'text' | 'number';
+}
+
+const TextField = ({ type = 'text', className, ...props }: TextFieldProps) => {
+  return (
+    <input
+      {...props}
+      inputMode={type === 'number' ? 'decimal' : undefined}
+      pattern={type === 'number' ? '[0-9]*' : undefined}
+      type={type}
+      className={cn(
+        'border-border-default rounded-xl border bg-white py-5 pr-[1.875rem] pl-[1.375rem] text-base font-medium',
+        'placeholder:text-base placeholder:font-medium placeholder:text-[#B1B7C1]',
+        className
+      )}
+    />
+  );
+};
+
+export default TextField;
