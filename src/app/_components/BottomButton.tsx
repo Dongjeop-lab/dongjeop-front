a089b6e69c46261922f4215c9666478f684dd5c9
yujeong <yujeong.choung@gmail.com>
2025-10-03 17:20:27 +0900

    feat: 하단 버튼 컴포넌트에 네비게이션 기능 추가 및 경로 상수 정의

--- src/app/_components/BottomButton.tsx
+++ src/app/_components/BottomButton.tsx
@@ -1,12 +1,33 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+
+import { BROWSER_PATH } from '@/lib/path';
+
 const BottomButton = () => {
+  const router = useRouter();
+
+  const handleNavigateToAccessibilityQuiz = () => {
+    router.push(`${BROWSER_PATH.QUIZ.DEFAULT}?step=1`);
+  };
+
+  const handleNavigateToUpload = () => {
+    router.push(BROWSER_PATH.LABEL.UPLOAD);
+  };
   return (
     <div className='fixed right-0 bottom-0 left-0 flex w-full flex-col items-center gap-2 bg-gradient-to-t from-white from-60% to-transparent px-[1.25rem] py-[2.5rem]'>
-      <button className='bg-secondary text-secondary-foreground h-[3.125rem] w-full rounded-[0.75rem] px-[2rem] py-[0.5rem] text-[1rem] leading-[100%] font-semibold tracking-[-0.015em]'>
+      <button
+        type='button'
+        onClick={handleNavigateToAccessibilityQuiz}
+        className='bg-secondary text-secondary-foreground h-[3.125rem] w-full rounded-[0.75rem] px-[2rem] py-[0.5rem] text-[1rem] leading-[100%] font-semibold tracking-[-0.015em]'
+      >
         접근성 퀴즈 풀어보기
       </button>
-      <button className='bg-primary h-[3.125rem] w-full rounded-[0.75rem] px-[2rem] py-[0.5rem] text-[1rem] leading-[100%] font-semibold tracking-[-0.015em] text-white'>
+      <button
+        type='button'
+        onClick={handleNavigateToUpload}
+        className='bg-primary h-[3.125rem] w-full rounded-[0.75rem] px-[2rem] py-[0.5rem] text-[1rem] leading-[100%] font-semibold tracking-[-0.015em] text-white'
+      >
         1분만에 실내 사진 올리기
       </button>
     </div>
