c846f18828c45d153fb03d8e053f347b5e11a7f0
Hyerim Yang <lovelimi1113@gmail.com>
2025-10-05 04:44:57 +0900

    fix: 배열 타입 쿼리 파라미터 처리 로직 추가
    - Next.js에서 중복 쿼리 파라미터 발생 시 배열로 전달되는 케이스 처리

--- src/lib/queryParams.ts
+++ src/lib/queryParams.ts
@@ -3,5 +3,8 @@ import { SOURCE, SourceType } from '@/types/common';
 export const getSourceParam = (
   params: Record<string, string | string[] | undefined>
 ): SourceType => {
-  return params.source === SOURCE.SHARE ? SOURCE.SHARE : SOURCE.NORMAL;
+  const source = Array.isArray(params.source)
+    ? params.source[0]
+    : params.source;
+  return source === SOURCE.SHARE ? SOURCE.SHARE : SOURCE.NORMAL;
 };
