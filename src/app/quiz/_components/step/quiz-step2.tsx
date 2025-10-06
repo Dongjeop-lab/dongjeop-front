53cfcb4435cc57c353098b57c4fca79f85d0e9c0
Pridesd <whwotjr98@naver.com>
2025-10-06 19:34:34 +0900

    feat: quiz-step 컴포넌트 기본 정의

---
+++ src/app/quiz/_components/step/quiz-step2.tsx
@@ -0,0 +1,5 @@
+import { QuizStepProps } from '../../_types/quiz-step';
+
+export const QuizStep2 = ({}: QuizStepProps) => {
+  return <>QuizStep2</>;
+};
---
+++ src/app/quiz/_components/step/quiz-step3.tsx
@@ -0,0 +1,5 @@
+import { QuizStepProps } from '../../_types/quiz-step';
+
+export const QuizStep3 = ({}: QuizStepProps) => {
+  return <>QuizStep3</>;
+};
