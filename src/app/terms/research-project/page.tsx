3c9628408f285b8d8f16249e7d763c04a8f3696b
yujeong <yujeong.choung@gmail.com>
2025-10-08 17:12:29 +0900

    feat: 연구 프로젝트 참여 약관 추가
    - 새로운 연구 프로젝트 참여 약관 상수 파일 생성
    - 연구 프로젝트 참여 약관 페이지 컴포넌트 구현

---
+++ src/app/terms/research-project/page.tsx
@@ -0,0 +1,8 @@
+import TermsContentComponent from '../_components/terms-content';
+import { RESEARCH_PROJECT_TERMS } from '../_constants/research-project';
+
+const ResearchProjectTermsPage = () => {
+  return <TermsContentComponent content={RESEARCH_PROJECT_TERMS} />;
+};
+
+export default ResearchProjectTermsPage;
