af6a32f5151b32a7b3a16d4f709c1d68fc371f10
yujeong <yujeong.choung@gmail.com>
2025-10-08 17:08:50 +0900

    feat: 피드백 수집 약관 추가
    - 피드백 수집을 위한 새로운 약관 상수 파일 생성
    - 피드백 수집 약관 페이지 컴포넌트 구현

---
+++ src/app/terms/feedback-collection/page.tsx
@@ -0,0 +1,8 @@
+import TermsContentComponent from '../_components/terms-content';
+import { FEEDBACK_COLLECTION_TERMS } from '../_constants/feedback-collection';
+
+const FeedbackCollectionTermsPage = () => {
+  return <TermsContentComponent content={FEEDBACK_COLLECTION_TERMS} />;
+};
+
+export default FeedbackCollectionTermsPage;
