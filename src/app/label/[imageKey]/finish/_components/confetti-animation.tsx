105fc769d7550a2968840eeaa4dcae3569935311
Hyerim Yang <lovelimi1113@gmail.com>
2025-10-09 17:35:22 +0900

    feat: 컨페티 애니메이션 컴포넌트 추가 및 Web Component 타입 설정

---
+++ src/app/label/[imageKey]/finish/_components/confetti-animation.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import('@lottiefiles/dotlottie-wc');
+
+export const ConfettiAnimation = () => {
+  return (
+    <dotlottie-wc
+      src='https://lottie.host/5d437f97-de32-4fb4-9f64-5b4a10f99c10/CS8eF9ZJqt.lottie'
+      style={{ width: '300px', height: '300px' }}
+      autoplay
+      loop
+    ></dotlottie-wc>
+  );
+};
