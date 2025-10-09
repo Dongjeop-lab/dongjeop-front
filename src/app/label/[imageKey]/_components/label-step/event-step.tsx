f3704ca10cb2e0d55cda0e79ff027734eb4e11dc
Pridesd <whwotjr98@naver.com>
2025-10-09 22:49:31 +0900

    fix: remove unuse lint

--- src/app/label/[imageKey]/_components/label-step/event-step.tsx
+++ src/app/label/[imageKey]/_components/label-step/event-step.tsx
@@ -9,19 +9,17 @@ interface EventStepProps {
 
 export const EventStep = ({ imageKey }: EventStepProps) => {
   return (
-    <>
-      <main className='mb-20'>
-        <h1 className='mb-8 flex flex-col items-center gap-3'>
-          <span className='text-18-semibold text-primary'>EVENT</span>
-          <span className='text-26-bold text-center'>
-            마지막으로 이벤트 참여를 위한
-            <br />
-            정보를 입력해주세요
-          </span>
-        </h1>
-        <EventFormSection imageKey={imageKey} />
-        <EventIntroSection />
-      </main>
-    </>
+    <main className='mb-20'>
+      <h1 className='mb-8 flex flex-col items-center gap-3'>
+        <span className='text-18-semibold text-primary'>EVENT</span>
+        <span className='text-26-bold text-center'>
+          마지막으로 이벤트 참여를 위한
+          <br />
+          정보를 입력해주세요
+        </span>
+      </h1>
+      <EventFormSection imageKey={imageKey} />
+      <EventIntroSection />
+    </main>
   );
 };
