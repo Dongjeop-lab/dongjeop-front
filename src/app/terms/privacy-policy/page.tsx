3258fd77dd2573c5b3d4685c0aaf7c85d9569d55
Jae-seok Jo (조재석) <whwotjr98@naver.com>
2025-10-23 17:09:54 +0900

    feat: 약관 추가 (#107)

---
+++ src/app/terms/privacy-policy/page.tsx
@@ -0,0 +1,9 @@
+import { TERMS } from '@/lib/terms';
+
+import TermsContentComponent from '../_components/terms-content';
+
+const ProvacyPolicyTermsPage = () => {
+  return <TermsContentComponent content={TERMS.PRIVACY_POLICY} />;
+};
+
+export default ProvacyPolicyTermsPage;
